1809773f8e46dfdc54c22ca6d48875ee3dc320f0
Eliot Chern <eliotchern@gmail.com>
2021-02-16 02:28:43 -0800

    create circles

--- Leaflet-Step-1/static/app.js
+++ Leaflet-Step-1/static/app.js
@@ -1,65 +1,70 @@
 // Store our API endpoint inside queryUrl
-const queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson";
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson";
 
-
-
-// Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
-  // Once we get a response, send the data.features object to the createFeatures function
-  createFeatures(data.features);
+// Create map object
+var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5
 });
 
-function createFeatures(earthquakeData) {
-
-  // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place and time of the earthquake
-  function onEachFeature(feature, layer) {
-    layer.bindPopup("<h2>" + feature.properties.place +
-      "</h2><hr><p>" + new Date(feature.properties.time) + "</p>");
-  }
-
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
-  var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
-
-  // Sending our earthquakes layer to the createMap function
-  createMap(earthquakes);
-}
-
-function createMap(earthquakes) {
-
-  // Define streetmap and darkmap layers
-  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+// Add light map tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "light-v10",
     accessToken: API_KEY
-  });
+}).addTo(myMap);
+  
+// Grabbing our GeoJSON data..
+d3.json(queryUrl, function(data) {
+    function mapStyle(feature) {
+        return {
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: mapColor(feature.properties.mag),
+            color: "#000000",
+            radius: mapRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+        };
+    }
 
+    function mapColor(mag) {
+        switch (true) {
+            case mag > 5:
+                return "#ea2c2c";
+            case mag > 4:
+                return "#eaa92c";
+            case mag > 3:
+                return "#d5ea2c";
+            case mag > 2:
+                return "#92ea2c";
+            case mag > 1:
+                return "#2ceabf";
+            default:
+                return "#2c99ea";
+        }
+    }
+    
+    function mapRadius(mag) {
+        if (mag === 0) {
+            return 1;
+        }
+        return mag * 4;
+    }
 
-  // Define a baseMaps object to hold our base layers
-
-
-  // Create overlay object to hold our overlay layer
-
-
-  // Create our map, giving it the streetmap and earthquakes layers to display on load
-  var myMap = L.map("map", {
-    center: [
-      37.09, -95.71
-    ],
-    zoom: 5,
-    layers: [lightmap, earthquakes]
-  });
-
-  // Create a layer control
-  // Pass in our baseMaps and overlayMaps
-  // Add legend
-  var legend = L.control({position: 'bottomright'});
-
-  legend.addTo(myMap);
-}
-
-
+    // Creating a geoJSON layer with the retrieved data
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng) {
+            return L.circleMarker(latlng);
+        },
+      
+        style: mapStyle,
+      
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup(`<h2>${feature.properties.place}</h2>
+            <hr><p>Magnitude: ${feature.properties.mag}<br>${new Date(feature.properties.time)}</p>`);      
+        }
+    }).addTo(myMap);
+});
+  
